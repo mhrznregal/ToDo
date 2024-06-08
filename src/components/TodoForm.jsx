b51a77af7babeda6497e848139d437a9b4852bc5
unknown <spookymhrzn@gmail.com>
2024-06-08 22:26:44 +0545

    first commit

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import { TodoProvider, useTodo } from "../context/TodoContext";
+
+function TodoForm() {
+  const [todo, setTodo] = useState("");
+  const { addTodo } = useTodo();
+
+  const add = (e) => {
+    e.preventDefault();
+
+    if (!todo) return;
+    addTodo({ todo, completed: false });
+    setTodo("");
+  };
+  return (
+    <form onSubmit={add} className="flex">
+      <input
+        type="text"
+        placeholder="Write Todo..."
+        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+      >
+        Add
+      </button>
+    </form>
+  );
+}
+
+export default TodoForm;
+export function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toogleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo === id
+          ? {
+              ...prevTodo,
+              completed: !prevTodo.completed,
+            }
+          : prevTodo
+      )
+    );
+  };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toogleComplete }}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
+  );
+}
